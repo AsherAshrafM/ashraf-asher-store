87700b466832cc1f16f15da53eadd3c4b1831770
Borge <borge@milkyway.com>
2022-10-26 15:34:27 +0800

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,46 @@
-// import Link from 'next/link';
-import styles from "../styles/Home.module.css";
+import { sidenavs } from '@/lib/sidenavs';
+import Link from 'next/link';
 
 export default function Page() {
   return (
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <div className="space-y-6">
+      <div className="space-y-8 text-white">
+        {sidenavs
+          .filter((section) =>
+            section.items.some((x) => typeof x.isDisabled === 'undefined'),
+          )
+          .map((section) => {
+            return (
+              <div key={section.name} className="space-y-3">
+                <div className="text-xs font-semibold uppercase tracking-wider text-zinc-500">
+                  {section.name}
+                </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+                <div className="grid grid-cols-2 gap-5">
+                  {section.items
+                    .filter((item) => !item.isDisabled)
+                    .map((item) => {
+                      return (
+                        <Link
+                          href={`/${item.slug}`}
+                          key={item.name}
+                          className="block space-y-1.5 rounded-lg border border-white/10 px-4 py-3 hover:border-white/20"
+                        >
+                          <div>{item.name}</div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                          {item.description ? (
+                            <div className="line-clamp-3 text-sm text-zinc-400">
+                              {item.description}
+                            </div>
+                          ) : null}
+                        </Link>
+                      );
+                    })}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 }
