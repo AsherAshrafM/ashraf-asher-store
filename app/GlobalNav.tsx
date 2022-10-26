95bf02acad208d402ea52e2949006d9ac8dfe545
Borge <borge@milkyway.com>
2022-10-26 12:44:18 +0800

    first commit

---
+++ app/GlobalNav.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+// import clsx from 'clsx';
+// import { useSelectedLayoutSegments } from 'next/navigation';
+import Link from "next/link";
+
+export default function GlobalNav() {
+  //   const [selectedLayoutSegments] = useSelectedLayoutSegments();
+
+  return (
+    <div>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
